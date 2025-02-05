6eec5f1799e2ddc46494e7f0b82c5d843f665a30
황제연 <kingpele9018@gmail.com>
2025-02-05 10:16:46 +0900

    Time: 1 ms (52.21%), Space: 41.6 MB (76.92%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -2,32 +2,23 @@ class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
         if(s1.equals(s2)){
             return true;
-        }else{
-            int n = s1.length();
-            for(int i=0; i<n; i++){
-                
-                for(int j=i+1; j<n; j++){
-                    char[] arr = s1.toCharArray();
-                    char[] arr2 = s2.toCharArray();
-                    char tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
-                    char tmp2 = arr2[i];
-                    arr2[i] = arr2[j];
-                    arr2[j] = tmp2;
-                    String s3 = new String(arr);
-                    String s4 = new String(arr2);
-                    // System.out.println(s3);
-                    System.out.println(s4);
-                    if(s3.equals(s2)){
-                        return true;
-                    }
-                    if(s4.equals(s1)){
-                        return true;
-                    }
+        }
+        int n = s1.length();
+        char[] s1Frq = new char[26];
+        char[] s2Frq = new char[26];
+        int diff = 0;
+        for(int i=0; i<n; i++){
+            char tmp1 = s1.charAt(i);
+            char tmp2 = s2.charAt(i);
+            if(tmp1 != tmp2){
+                diff++;
+                if(diff > 2){
+                    return false;
                 }
             }
+        s1Frq[tmp1-'a']++;
+        s2Frq[tmp2-'a']++;
         }
-        return false;
+        return Arrays.equals(s1Frq, s2Frq);
     }
 }
