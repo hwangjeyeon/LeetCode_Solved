3ce893f1122771091f3f094eac517ce4ae0583e3
황제연 <kingpele9018@gmail.com>
2025-03-09 09:12:07 +0900

    Time: 3 ms (86.87%), Space: 55.7 MB (99.72%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int[] arr = new int[colors.length + k - 1];
+        for(int i=0; i<colors.length; i++){
+            arr[i] = colors[i];
+        }
+        for(int i=0; i<k-1; i++){
+            arr[colors.length+i] = colors[i];
+        }
+
+        int len = arr.length;
+        int res = 0;
+        int left = 0;
+        int right = 1;
+        while(right < len){
+            if(arr[right] == arr[right-1]){
+                left = right;
+                right++;
+                continue;
+            }
+            right++;
+            if(right - left < k){
+                continue;
+            }
+            res++;
+            left++;
+        }
+        return res;
+    }
+}
