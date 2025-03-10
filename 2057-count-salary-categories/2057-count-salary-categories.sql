af440888e63b741d4195ff5d5091f0e3218f4112
황제연 <kingpele9018@gmail.com>
2025-03-10 15:36:51 +0900

    Time: 2932 ms (36.14%), Space: 0B (100%) - LeetHub

---
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -0,0 +1,8 @@
+select 'Low Salary' as category, count(if(income < 20000, 1, null)) as accounts_count
+from Accounts
+union all
+select 'Average Salary' as category, count(if(income >= 20000 and income <= 50000, 1, null)) as accounts_count
+from Accounts
+union all
+select 'High Salary' as category, count(if(income > 50000, 1, null)) as accounts_count
+from Accounts
