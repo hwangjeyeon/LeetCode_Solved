e1718814d4b99a2be12cf6f3d3e0717df13c3da3
황제연 <kingpele9018@gmail.com>
2025-03-21 00:34:08 +0900

    Time: 19 ms (72.95%), Space: 56.1 MB (74.16%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.java
+++ 2690-house-robber-iv/2690-house-robber-iv.java
@@ -1,27 +1,27 @@
 class Solution {
     public int minCapability(int[] nums, int k) {
-        int min = 1;
-        int max = 1;
+        int left = 1;
+        int right = 1;
         for(int i=0; i<nums.length; i++){
-            max = Math.max(nums[i], max);
+            right = Math.max(right, nums[i]);
         }
+
         int ans = Integer.MAX_VALUE;
-        while(min <= max){
-            int mid = (min + max) / 2;
-            int pos = 0;
-            for(int i=0; i< nums.length; i++){
+        while(left <= right){
+            int mid = (left + right) / 2;
+            int count = 0;
+            for(int i=0; i<nums.length; i++){
                 if(nums[i] <= mid){
-                    pos++;
+                    count++;
                     i++;
                 }
             }
-            if(pos >= k){
-                max = mid-1;
+            if(count >= k){
+                right = mid - 1;
                 ans = Math.min(ans, mid);
             }else{
-                min = mid + 1;
+                left = mid + 1;
             }
-
         }
         return ans;
     }
