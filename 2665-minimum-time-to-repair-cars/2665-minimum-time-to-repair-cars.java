d16f52b3dc41cf7beb58f61ba8f73b7a321db7fd
황제연 <kingpele9018@gmail.com>
2025-03-21 00:03:56 +0900

    Time: 4 ms (98.55%), Space: 56.3 MB (47.06%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -2,14 +2,14 @@ class Solution {
     public long repairCars(int[] ranks, int cars) {
         int min = ranks[0];
         int max = ranks[0];
-        for(int i: ranks){
-            min = Math.min(min, i);
-            max = Math.max(max, i);
+        for(int rank : ranks){
+            min = Math.min(min, rank);
+            max = Math.max(max, rank);
         }
-        int[] arr = new int[max+1];
-        for(int i : ranks){
-            min = Math.min(min, i);
-            arr[i]++;
+        int[] freq = new int[max + 1];
+        for(int rank : ranks){
+            min = Math.min(min, rank);
+            freq[rank]++;
         }
 
         long left = 1;
@@ -18,14 +18,14 @@ class Solution {
         while(left < right){
             long mid = (left + right) / 2;
             long count = 0;
-            for(int i=1; i<=max; i++){
-                count+= arr[i] * (long) Math.sqrt(mid / (long) i);
+            for(int i=1; i<max + 1; i++){
+                count += freq[i] * (long) Math.sqrt(mid / (long) i);
             }
 
             if(count >= cars){
                 right = mid;
             }else{
-                left = mid+1;
+                left = mid + 1;
             }
         }
         return left;
