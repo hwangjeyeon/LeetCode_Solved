e80ebea42b3dc8d31405908df7342ee62cac75d8
황제연 <kingpele9018@gmail.com>
2025-03-23 00:30:27 +0900

    Time: 14 ms (39.21%), Space: 45.7 MB (14.21%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -1,29 +1,45 @@
 class Solution {
+    static List<Integer>[] list;
+    static int ans = 0;
+    static Set<Integer> visited;
+    static int adj;
+    static int node;
     public int countCompleteComponents(int n, int[][] edges) {
-        List<Integer>[] list = new List[n];
-        Map<List<Integer>, Integer> map = new HashMap<>();
+        int len = edges.length;
+        list = new List[n];
+        visited = new HashSet<>();
         for(int i=0; i<n; i++){
             list[i] = new ArrayList<>();
-            list[i].add(i);
         }
 
-        for(int i=0; i<edges.length; i++){
+        for(int i=0; i<len; i++){
             list[edges[i][0]].add(edges[i][1]);
             list[edges[i][1]].add(edges[i][0]);
         }
-
+        ans = 0;
         for(int i=0; i<n; i++){
-            List<Integer> adj = list[i];
-            Collections.sort(adj);
-            map.put(adj, map.getOrDefault(adj, 0) + 1);
-        }
-        int count = 0;
-        for(Map.Entry<List<Integer>, Integer> edge : map.entrySet()){
-            if(edge.getKey().size() == map.get(edge.getKey())){
-                count++;
+            if(visited.contains(i)){
+                continue;
+            }
+            adj = 0;
+            node = 0;
+            dfs(i);
+            if(adj == (node) * (node-1)){
+                ans++;
             }
         }
-        return count;
+        return ans;
+    }
+    private static void dfs(int now){
+        visited.add(now);
+        node++;
+        adj += list[now].size();
+        for(Integer edge : list[now]){
+            if(!visited.contains(edge)){
+                dfs(edge);
+            }
+           
+        }
     }
 
 }
