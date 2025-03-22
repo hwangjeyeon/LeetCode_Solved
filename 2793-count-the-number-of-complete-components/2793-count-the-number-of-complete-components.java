31077424c5ffcf55f806b794ff21666afa0a5111
황제연 <kingpele9018@gmail.com>
2025-03-23 00:09:50 +0900

    Time: 27 ms (14.38%), Space: 45.8 MB (7.19%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -1,26 +1,25 @@
 class Solution {
     public int countCompleteComponents(int n, int[][] edges) {
         List<Integer>[] list = new List[n];
-        Map<List<Integer>, Integer> freq = new HashMap<>();
-
+        Map<List<Integer>, Integer> map = new HashMap<>();
         for(int i=0; i<n; i++){
             list[i] = new ArrayList<>();
             list[i].add(i);
         }
-        for(int[] edge : edges){
-            list[edge[0]].add(edge[1]);
-            list[edge[1]].add(edge[0]);
+
+        for(int i=0; i<edges.length; i++){
+            list[edges[i][0]].add(edges[i][1]);
+            list[edges[i][1]].add(edges[i][0]);
         }
 
-        for(int i=0; i< n; i++){
+        for(int i=0; i<n; i++){
             List<Integer> adj = list[i];
             Collections.sort(adj);
-            freq.put(adj, freq.getOrDefault(adj, 0) + 1);
+            map.put(adj, map.getOrDefault(adj, 0) + 1);
         }
-
         int count = 0;
-        for(Map.Entry<List<Integer>, Integer> entry : freq.entrySet()){
-            if(entry.getKey().size() == entry.getValue()){
+        for(Map.Entry<List<Integer>, Integer> edge : map.entrySet()){
+            if(edge.getKey().size() == map.get(edge.getKey())){
                 count++;
             }
         }
