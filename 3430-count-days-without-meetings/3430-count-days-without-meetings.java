88ce6645e27f1d6c1a6a04dc91e89129a19872ce
황제연 <kingpele9018@gmail.com>
2025-03-27 22:46:52 +0900

    Time: 233 ms (8.27%), Space: 99.1 MB (82.1%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -1,26 +1,26 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        int pre = 0;
-        int free = 0;
-        int prev = days;
+        int ans = 0; // 휴일
+        int prev = days; // 이전 날짜
+        int count = 0; // 현재 회의 수
         for(int[] meeting : meetings){
             prev = Math.min(prev, meeting[0]);
             map.put(meeting[0], map.getOrDefault(meeting[0], 0) + 1);
-            map.put(meeting[1] + 1, map.getOrDefault(meeting[1] + 1, 0) - 1);
+            map.put(meeting[1]+1, map.getOrDefault(meeting[1]+1, 0) - 1);
         }
 
-        free += (prev - 1);
-        for(Map.Entry<Integer, Integer> now : map.entrySet()){
-            int cur = now.getKey();
-            if(pre == 0){
-                free += (cur - prev);
+        ans += (prev - 1);
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int cur = entry.getKey();
+            if(count == 0){
+                ans += (cur - prev);
             }
-            pre += now.getValue();
+            count += entry.getValue();
             prev = cur;
         }
-        free += days - prev + 1;
+        ans += days - prev + 1;
         
-        return free;
+        return ans;
     }
 }
