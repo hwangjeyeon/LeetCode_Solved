e5a30b4c9f5a68a5de7da33adf08a7d5af6d7da4
황제연 <kingpele9018@gmail.com>
2025-05-20 22:24:44 +0900

    Time: 281 ms (96.91%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
-select patient_id, patient_name, conditions from Patients
-where conditions like 'DIAB1%'or conditions like '% DIAB1%'
+/* Write your T-SQL query statement below */
+SELECT patient_id, patient_name, conditions FROM PATIENTS 
+WHERE CONDITIONS LIKE 'DIAB1%' OR CONDITIONS LIKE '% DIAB1%'
